c457314680527f171287ce6dd3a0c79545910ef4
warappa <spoiltnr1@gmx.at>
2021-07-19 00:40:16 +0200

    Move MainForm to own file

---
+++ csharp/MainForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using static Refterm.NativeWindows;
+
+namespace Refterm
+{
+    public class MainForm : Form
+    {
+        private Thread terminalThread;
+        uint terminalThreadId;
+
+
+        [STAThread]
+        private void ThreadMethod(object? handle)
+        {
+            terminalThreadId = NativeWindows.GetCurrentThreadId();
+            var terminal = new Terminal((IntPtr)handle);
+        }
+
+        public MainForm()
+        {
+            terminalThread = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            terminalThread.Start(Handle);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            var type = (WndMsg)m.Msg;
+            if (terminalThreadId > 0 &&
+                (type == WndMsg.WM_CHAR ||
+                type == WndMsg.WM_KEYDOWN ||
+                type == WndMsg.WM_QUIT ||
+                type == WndMsg.WM_SIZE))
+            {
+                NativeWindows.PostThreadMessage(terminalThreadId, (uint)m.Msg, m.WParam, m.LParam);
+            }
+            else
+            {
+                if (type == WndMsg.WM_SYSCOMMAND && 
+                    m.WParam.ToInt32() == (int)SysCommands.SC_CLOSE)
+                {
+                    NativeWindows.PostThreadMessage(terminalThreadId, (uint)WndMsg.WM_QUIT, IntPtr.Zero, IntPtr.Zero);
+                }
+
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.Win32.SafeHandles;
-using System;
-using System.Drawing;
-using System.IO.MemoryMappedFiles;
-using System.Threading;
+﻿using System;
 using System.Windows.Forms;
-using static Refterm.NativeWindows;
 
 namespace Refterm
 {
@@ -16,53 +11,12 @@ namespace Refterm
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Form1 mainForm = new Form1();
+
+            var mainForm = new MainForm();
             mainForm.Width = 612;
             mainForm.Height = 612;
+
             Application.Run(mainForm);
-        }
-
-    }
-
-    public class Form1 : Form
-    {
-        private Thread terminalThread;
-        uint terminalThreadId;
-
-
-        [STAThread]
-        private void ThreadMethod(object? handle)
-        {
-            terminalThreadId = NativeWindows.GetCurrentThreadId();
-            var terminal = new Terminal((IntPtr)handle);
-        }
-
-        public Form1()
-        {
-            terminalThread = new Thread(new ParameterizedThreadStart(ThreadMethod));
-            terminalThread.Start(Handle);
-        }
-        protected override void WndProc(ref Message m)
-        {
-            var type = (WndMsg)m.Msg;
-            if (terminalThreadId > 0 &&
-                (type == WndMsg.WM_CHAR ||
-                type == WndMsg.WM_KEYDOWN ||
-                type == WndMsg.WM_QUIT ||
-                type == WndMsg.WM_SIZE))
-            {
-                NativeWindows.PostThreadMessage(terminalThreadId, (uint)m.Msg, m.WParam, m.LParam);
-            }
-            else
-            {
-                if (type == WndMsg.WM_SYSCOMMAND && 
-                    m.WParam.ToInt32() == (int)SysCommands.SC_CLOSE)
-                {
-                    NativeWindows.PostThreadMessage(terminalThreadId, (uint)WndMsg.WM_QUIT, IntPtr.Zero, IntPtr.Zero);
-                }
-
-                base.WndProc(ref m);
-            }
         }
     }
 }
